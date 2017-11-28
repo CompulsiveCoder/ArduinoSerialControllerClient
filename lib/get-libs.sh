1f41388f5c082fd81ece459a62fa5bb03f2df776
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 02:46:44 +0000

    Moved duinocom.core from submodule to nuget package

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -5,12 +5,16 @@ echo "Dir: $PWD"
 
 #cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
 
-#NUGET_FILE="nuget.exe"
+NUGET_FILE="nuget.exe"
 
-#if [ ! -f "$NUGET_FILE" ];
-#then
-#    wget http://nuget.org/nuget.exe
-#fi
+if [ ! -f "$NUGET_FILE" ];
+then
+    wget http://nuget.org/nuget.exe
+fi
+
+if [ ! -d "duinocom.core.1.0.0" ]; then
+    mono nuget.exe install duinocom.core -version 1.0.0
+fi
 
 #if [ ! -d "NUnit.2.6.4" ]; then
 #    mono nuget.exe install nunit -version 2.6.4
