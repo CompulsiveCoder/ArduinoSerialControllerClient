7cf583e9666d6f91e55bcdf8e5d6e02d3ac39afc
CompulsiveCoder <compulsivecoder@gmail.com>
2017-11-28 01:31:25 +0000

    Disabled retrieval of nunit because not tests are implemented yet

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -3,19 +3,19 @@
 echo "Getting libraries for ArduinoSerialControllerClient project"
 echo "Dir: $PWD"
 
-cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
+#cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
 
-NUGET_FILE="nuget.exe"
+#NUGET_FILE="nuget.exe"
 
-if [ ! -f "$NUGET_FILE" ];
-then
-    wget http://nuget.org/nuget.exe
-fi
+#if [ ! -f "$NUGET_FILE" ];
+#then
+#    wget http://nuget.org/nuget.exe
+#fi
 
-if [ ! -d "NUnit.2.6.4" ]; then
-    mono nuget.exe install nunit -version 2.6.4
-fi
+#if [ ! -d "NUnit.2.6.4" ]; then
+#    mono nuget.exe install nunit -version 2.6.4
+#fi
 
-if [ ! -d "NUnit.Runners.2.6.4" ]; then
-    mono nuget.exe install nunit.runners -version 2.6.4
-fi
+#if [ ! -d "NUnit.Runners.2.6.4" ]; then
+#    mono nuget.exe install nunit.runners -version 2.6.4
+#fi
