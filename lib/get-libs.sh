b9949730947deb72d9cc265a5331ba205eddf38c
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-06 20:39:17 +1000

    Updated duinocom version

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -3,18 +3,17 @@
 echo "Getting libraries for ArduinoSerialControllerClient project"
 echo "Dir: $PWD"
 
-#cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
-
 NUGET_FILE="nuget.exe"
 
 if [ ! -f "$NUGET_FILE" ];
 then
     wget http://nuget.org/nuget.exe
-    mono nuget.exe update -self
 fi
 
-if [ ! -d "duinocom.core.1.0.4" ]; then
-    mono nuget.exe install duinocom.core -version 1.0.4
+mono nuget.exe update -self
+
+if [ ! -d "duinocom.core.1.0.6" ]; then
+    mono nuget.exe install duinocom.core -version 1.0.6
 fi
 
 #if [ ! -d "NUnit.2.6.4" ]; then
