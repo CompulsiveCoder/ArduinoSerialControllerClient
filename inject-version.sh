e3b2a0cfa41b810daf3b8b371c2f60742b656957
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-20 06:11:18 +0000

    Added jenkinsfile and scripts

---
+++ inject-version.sh
@@ -0,0 +1,10 @@
+VERSION=$(cat version.txt)
+BUILD_NUMBER=$(cat buildnumber.txt)
+
+FULL_VERSION="$VERSION-$BUILD_NUMBER"
+
+# TODO: Implement
+
+#SOURCE_FILE="?"
+
+#sed -i "s/#define VERSION .*/#define VERSION \"$FULL_VERSION\"/" $SOURCE_FILE
