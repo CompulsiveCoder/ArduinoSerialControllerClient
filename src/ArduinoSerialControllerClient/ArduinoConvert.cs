651ca933eeb6f0c17e0a7489572ff070cded21a8
CompulsiveCoder <compulsivecoder@gmail.com>
2019-02-25 00:14:21 +0000

    Cleaned up code

--- src/ArduinoSerialControllerClient/ArduinoConvert.cs
+++ src/ArduinoSerialControllerClient/ArduinoConvert.cs
@@ -1,44 +1,39 @@
 using System;
-//using NUnit.Framework;
-//using duinocom;
-//using System.Threading;
-//using ArduinoSerialControllerClient;
-//using System.Collections.Generic  ;
 
 namespace ArduinoSerialControllerClient
 {
-	static public class ArduinoConvert
-	{
-        static public int PercentageToPWM(int percentageValue)
+    static public class ArduinoConvert
+    {
+        static public int PercentageToPWM (int percentageValue)
         {
-            var fractionValue = (float)percentageValue/(float)100;
+            var fractionValue = (float)percentageValue / (float)100;
             
-            var pwmValue = fractionValue*(float)255;
+            var pwmValue = fractionValue * (float)255;
             
             return (int)pwmValue;
         }
-        
-        static public int PercentageToAnalog(int percentageValue)
+
+        static public int PercentageToAnalog (int percentageValue)
         {
-            var fractionValue = (float)percentageValue/(float)100;
+            var fractionValue = (float)percentageValue / (float)100;
             
-            var pwmValue = fractionValue*(float)1024;
+            var pwmValue = fractionValue * (float)1024;
             
             return (int)pwmValue;
         }
-        
-        static public int AnalogToPercentage(int analogValue)
+
+        static public int AnalogToPercentage (int analogValue)
         {
-            var fractionValue = (float)analogValue/(float)1024;
+            var fractionValue = (float)analogValue / (float)1024;
             
-            var percentageValue = fractionValue*(float)100;
+            var percentageValue = fractionValue * (float)100;
             
             return (int)percentageValue;
         }
-        
-        static public int ReversePercentage(int percentageValue)
+
+        static public int ReversePercentage (int percentageValue)
         {
-            return 100-percentageValue;
+            return 100 - percentageValue;
         }
-	}
+    }
 }
