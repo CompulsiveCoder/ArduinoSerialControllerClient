38fc67450244fc0d19f14742620862fa216247c1
CompulsiveCoder <compulsivecoder@gmail.com>
2019-12-14 12:48:53 +1100

    Updated duinocom library

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,6 +1,6 @@
 echo "Getting library files..."
 echo "  Dir: $PWD"
 
-bash install-package-from-github-release.sh CompulsiveCoder duinocom.core 1.2.0.25
+bash install-package-from-github-release.sh CompulsiveCoder duinocom.core 1.2.0.27
 
 echo "Finished getting library files."
